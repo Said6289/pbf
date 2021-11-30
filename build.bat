bdd9b7233c104c2e6d9a2b9789defa40805558fe
Said Al Attrach <alattrach.said@yahoo.com>
2021-11-30 14:51:05 +0100

    Change build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-cl /O2 /Zi /Fe:release\pbf.exe /I third_party\SDL2\include main.cpp /link /subsystem:windows /libpath:third_party\SDL2\lib_x64 Shell32.lib Opengl32.lib SDL2.lib SDL2main.lib
+cl /O2 /Zi /Fe:pbf.exe /I third_party\SDL2\include main.cpp /link /subsystem:console /libpath:third_party\SDL2\lib_x64 Shell32.lib Opengl32.lib SDL2.lib SDL2main.lib
